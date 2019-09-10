36166971a85ab2e0d925dade7700f091a5729c4a
Anudit Nagar <nagaranudit@gmail.com>
2019-09-10 08:56:52 +0530

    Ability to set Custom Secrets

--- com.py
+++ com.py
@@ -67,6 +67,10 @@ class BBSStringCommitmentScheme(CommitmentScheme):
         self.secret = [x.secret for x in self.schemes]
         return self.secret
 
+    def setSecret(self, sec):
+        self.secret = sec
+        return sec
+
     def commit(self, integer):
         # .zfill(len(self.schemes))
         binaryString = ''.join("0" + (format(ord(x), 'b')) for x in str(integer))
@@ -99,17 +103,15 @@ class BBSStringCommitmentVerifier(object):
 
 if __name__ == "__main__":
 
-    securityParameter = 600
+    securityParameter = 10
     oneWayPerm = bbs.bbs(securityParameter)
     hardcorePred = bbs.parity
     scheme = BBSStringCommitmentScheme(100, oneWayPerm, hardcorePred)
     verifier = BBSStringCommitmentVerifier(100, oneWayPerm, hardcorePred)
-
     # secrets = scheme.reveal()
-    # print(secrets)
 
     secrets = [45782, 34856, 9604, 8665, 28702, 37591, 30483, 33756, 9524, 14601, 42310, 6183, 24988, 6204, 3488, 39433, 34766, 46094, 19775, 23875, 40943, 11285, 6183, 24054, 25734, 5223, 6094, 14392, 16113, 43007, 21454, 46200, 17083, 6469, 30745, 20459, 24413, 21196, 16462, 19760, 10746, 42028, 41763, 12019, 42131, 32287, 39827, 4611, 26412, 2678, 38928, 8230, 11994, 1188, 25799, 19533, 30160, 967, 47, 26877, 19551, 12191, 31983, 41621, 22856, 13221, 32849, 29302, 2897, 6893, 5049, 10568, 26756, 1696, 26809, 17845, 47893, 11553, 6838, 31047, 9484, 6599, 34742, 43480, 45195, 31013, 31375, 17183, 24988, 6204, 3488, 39433, 34766, 46094, 19775, 23875, 40943, 11285, 6183, 24054, 25594, 24890, 46352, 16946, 36376, 18046, 45910, 1271, 9684, 42232, 14326, 5629, 1966]
-    print(f"SECRETS : {secrets}")
+    secrets = scheme.setSecret(secrets)
 
     comm = [(43254, 1), (40858, 1), (30679, 0), (21656, 1), (38196, 0), (35191, 1) , (40405, 1), (17899, 0), (10418, 0), (38617, 1), (31641, 0), (41633, 0), (48182, 0),(9506, 0), (37603, 1), (24082, 1)]
     print(f"COMMITMENTS : {comm}")
